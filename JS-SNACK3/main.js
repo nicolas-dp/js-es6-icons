30a84ce0436355bccbc9a0c9036a57544cb98101
Nicolas <wellcomenicolas@gmail.com>
2022-04-11 16:36:43 +0200

    Final JS Snack 3

---
+++ JS-SNACK3/main.js
@@ -0,0 +1,41 @@
+/* Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+Crea un nuovo array con la lista dei mammiferi. */
+
+const animali = [
+    { 
+        nome: 'leone', 
+        famiglia: 'felidi', 
+        classe: 'mammiferi' 
+    },
+    { 
+        nome: 'cane', 
+        famiglia: 'canidi', 
+        classe: 'mammiferi' 
+    },
+    { 
+        nome: 'gallina', 
+        famiglia: 'fasianidi', 
+        classe: 'uccelli' 
+    },
+  ]
+
+
+
+//Crea un nuovo array con la lista dei mammiferi.
+
+const animali_mammiferi = animali.filter(auto => auto.classe == "mammiferi");
+
+console.log(animali_mammiferi);
+
+
+
+
+
+
